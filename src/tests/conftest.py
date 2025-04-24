ad818321e4e1739b263a0db25d45ec2b78459d44
erikdeirdre <pwhite@delpwhite.org>
2025-04-24 16:48:02 -0400

    ignore migrations

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -39,7 +39,7 @@ def test_client():
 
 @pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
